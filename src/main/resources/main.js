01b1781bb06872916c7e5569e4f36b46dbe17491
Alex Shklianko <asl@enonic.com>
2018-03-19 17:02:38 +0300

    Session ID added (simple but enough for now)

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -4,6 +4,7 @@ var router = require('/lib/router')();
 var helper = require('/lib/helper');
 var swController = require('/lib/pwa/sw-controller');
 var siteTitle = 'AI Bot';
+var sessionId;
 
 var renderPage = function (pageName) {
     return function () {
@@ -27,8 +28,10 @@ var renderPage = function (pageName) {
 };
 
 var sendToRasa = function (params, method, action) {
+    var url = helper.getRasaUrl(sessionId) + action;
+
     var rasaResponse = httpClient.request({
-        url: helper.getRasaUrl() + action,
+        url: url,
         method: method,
         headers: {
             'Cache-Control': 'no-cache',
@@ -61,12 +64,23 @@ function rasaContinue(req) {
     var data = JSON.parse(req.params["data"]);
     var action = data.action;
     var events = data.events || [];
+    if (isRestart(events)) {
+        resetSessionId();
+    }
     log.info('RASA CONTINUE >>> action: ' + action + ', events: ' + JSON.stringify(events));
     var body = {
         "executed_action": action,
         "events": events
     };
     return sendToRasa(body, 'POST', 'continue');
+}
+
+function isRestart(events) {
+    return events.some(function (ev) { return ev.event === "restart" });
+}
+
+function resetSessionId() {
+    sessionId =  new Date().getTime();
 }
 
 router.get('/', renderPage('main.html'));
