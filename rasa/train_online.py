80fa35de6d908f83985627d6f50d389f417ca599
Pavel Milkevich <pavel.milkevich@gmail.com>
2018-08-22 13:47:04 +0300

    - added online training and visualization py files

---
+++ rasa/train_online.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+from rasa_core.agent import Agent
+from rasa_core.channels.console import ConsoleInputChannel
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.policies.keras_policy import KerasPolicy
+from rasa_core.policies.memoization import MemoizationPolicy
+
+logger = logging.getLogger(__name__)
+
+
+def run_online_training(input_channel, interpreter,
+                        domain_file="domain_remote.yml",
+                        training_data_file='data/stories.md'):
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(max_history=2), KerasPolicy()],
+                  interpreter=interpreter)
+
+    agent.train_online(training_data_file,
+                       input_channel=input_channel,
+                       batch_size=50,
+                       epochs=200,
+                       max_training_samples=300)
+
+    return agent
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level="INFO")
+    nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/current')
+    run_online_training(ConsoleInputChannel(), nlu_interpreter)
